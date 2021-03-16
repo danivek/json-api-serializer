638c4da397e84aa4ebecd65be27d7ad11c0333bf
Alex Zherdev <alex_zherdev@acl.com>
2021-03-16 15:18:35 -0700

    Fix crash in case conversion helpers.
    Resolves #118.

--- test/unit/helpers.test.js
+++ test/unit/helpers.test.js
@@ -1,35 +1,55 @@
 const { expect } = require('chai');
 
-const { set } = require('../../lib/helpers');
+const { set, toCamelCase, toSnakeCase, toKebabCase } = require('../../lib/helpers');
 
 describe('Helpers', () => {
-  it('should do nothing if it is not an object', () => {
-    const object = 1;
-    set(object, 'exist', true);
-    expect(object).to.equal(1);
+  describe('set', () => {
+    it('should do nothing if it is not an object', () => {
+      const object = 1;
+      set(object, 'exist', true);
+      expect(object).to.equal(1);
+    });
+
+    it('should set a value by path', () => {
+      const object = { exist: {} };
+      set(object, 'exist', true);
+      set(object, 'a[0].b.c', 4);
+      set(object, ['x', '0', 'y', 'z'], 5);
+      expect(object).to.deep.equal({
+        exist: true,
+        a: [
+          {
+            b: {
+              c: 4,
+            },
+          },
+        ],
+        x: [
+          {
+            y: {
+              z: 5,
+            },
+          },
+        ],
+      });
+    });
   });
 
-  it('should set a value by path', () => {
-    const object = { exist: {} };
-    set(object, 'exist', true);
-    set(object, 'a[0].b.c', 4);
-    set(object, ['x', '0', 'y', 'z'], 5);
-    expect(object).to.deep.equal({
-      exist: true,
-      a: [
-        {
-          b: {
-            c: 4,
-          },
-        },
-      ],
-      x: [
-        {
-          y: {
-            z: 5,
-          },
-        },
-      ],
+  describe('toCamelCase', () => {
+    it('should not choke on non-alpha characters', () => {
+      expect(toCamelCase('*')).to.equal('*');
+    });
+  });
+
+  describe('toSnakeCase', () => {
+    it('should not choke on non-alphanumeric characters', () => {
+      expect(toSnakeCase('*')).to.equal('*');
+    });
+  });
+
+  describe('toKebabCase', () => {
+    it('should not choke on non-alphanumeric characters', () => {
+      expect(toKebabCase('*')).to.equal('*');
     });
   });
 });
